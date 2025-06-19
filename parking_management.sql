c98f129d31ddca8906566b8ed25babfd53b4d41e
unknown <yahya@parkonic.com>
2025-06-19 10:59:35 +0400

    fixing sql schema syntex error

--- parking_management.sql
+++ parking_management.sql
@@ -114,7 +114,7 @@ CREATE TABLE `manual_reviews` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-- --------------------------------------------------------
+--------------------------------------------------------
 
 --
 -- Table structure for table `plate_logs`
